d27440d36f1987cc29264c41ad5321221890ddd8
Jean-Charles BONNARD <jc.bonnard@gyptis.fr>
2025-08-19 11:07:55 +0200

    19-08-2025 push dist for pages

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/versailles-points-counter/sw.js', { scope: '/versailles-points-counter/' })})}
